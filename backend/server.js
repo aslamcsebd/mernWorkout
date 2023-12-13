01e039ca73829641d91a24b2769b91edfb36d33e
aslamcsebd <aslamcsebd@gmail.com>
2023-12-13 12:48:50 +0600

    Express Router & API Routes

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,20 @@
 require('dotenv').config()
 port = process.env.PORT
 
-const express = require('express')
-
 // express app
+const express = require('express')
+const workoutRouters = require('./routes/workouts')
 const app = express()
 
 // middleware
+app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
-
 // routes
-app.get('/', (req, res) => {
-    res.json({msg: 'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRouters)
 
 // list of requests
 app.listen(port, () => {
---
+++ readme.md
@@ -0,0 +1,13 @@
+### backend
+
+- [Express app setup](https://www.youtube.com/watch?v=8DploTqLstE&list=PL4cUxeGkcC9iJ_KkrkBZWZRHVwnzLIoUE&index=2&ab_channel=NetNinja)
+- [Express Router & API Routes](https://www.youtube.com/watch?v=Ll6knx7sFis&list=PL4cUxeGkcC9iJ_KkrkBZWZRHVwnzLIoUE&index=3&ab_channel=NetNinja)
+- Postman api url
+
+    ```
+    $url = localhost:4000
+    Get:    $url/api/workouts/
+    Post:   $url/api/workouts/
+    Delete: $url/api/workouts/1010
+    Patch:  $url/api/workouts/1010
+	```
